f8d4738f3da8487ddf4c681047af238ab8945db2
Демшин Дмитрий Олегович <Demshin_DO@pf.vis.local>
2020-08-21 09:32:08 +0300

    add parametrisation for test of product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,10 +9,11 @@ class ProductPage(BasePage):
 
     def should_be_add_to_basket_message(self):
         item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        item_name += " has been added to your basket."
         assert self.is_element_present(*ProductPageLocators.ITEM_ADDED_TO_BASKET_MESSAGE),\
             "Adding to basket message is not present"
         message = self.browser.find_element(*ProductPageLocators.ITEM_ADDED_TO_BASKET_MESSAGE).text
-        assert item_name in message, "Adding to basket message does not contain name of item"
+        assert item_name == message, "Adding to basket message does not contain name of item"
 
     def should_be_total_price_of_basket(self):
         item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,22 @@
+import pytest
 from pages.product_page import ProductPage
-import time
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('url', ["?promo=offer0",
+                                 "?promo=offer1",
+                                 "?promo=offer2",
+                                 "?promo=offer3",
+                                 "?promo=offer4",
+                                 "?promo=offer5",
+                                 "?promo=offer6",
+                                 pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                 "?promo=offer8",
+                                 "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, url):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" + url
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_add_to_basket_message()
     page.should_be_total_price_of_basket()
-
-
